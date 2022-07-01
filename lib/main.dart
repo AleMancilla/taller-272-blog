21ce12ddff1ed825701e0abd729dca725fec2a35
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2022-07-01 01:45:41 -0400

    connect with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:blog_taller_base_de_datos/admin/presentation/pages/login_page.dart';
 import 'package:blog_taller_base_de_datos/article/presentation/pages/home_page.dart';
+import 'package:blog_taller_base_de_datos/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
